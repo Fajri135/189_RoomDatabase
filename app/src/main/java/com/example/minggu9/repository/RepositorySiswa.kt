cfe1a777a24269fb17c44952d458e45fa0f17ee1
Fajri135 <corex135@gmail.com>
2025-11-28 10:26:07 +0700

    menambahkan repository untuk pengelolaan data siswa dengan implementasi offline menggunakan Room

---
+++ app/src/main/java/com/example/minggu9/repository/RepositorySiswa.kt
@@ -0,0 +1,16 @@
+package com.example.minggu9.repository
+
+interface RepositoriSiswa {
+    fun getAllSiswaStream(): Flow<List<Siswa>>
+    suspend fun insertSiswa(siswa: Siswa)
+}
+
+
+class OfflineRepositoriSiswa(
+    private val siswaDao: SiswaDao
+) : RepositoriSiswa {
+
+    override fun getAllSiswaStream(): Flow<List<Siswa>> = siswaDao.getAllSiswa()
+
+    override suspend fun insertSiswa(siswa: Siswa) = siswaDao.insert(siswa)
+}
